f35b473ead80b1c2a26f4a79b65328e607966927
Harry Park <imharrypark@gmail.com>
2015-11-06 14:30:19 -0500

    create form for review; flash error messages if any.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,27 @@
 class ReviewsController < ApplicationController
   def index
+    @reviews = Review.all
   end
 
   def new
+    @review = Review.new
   end
+
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      redirect_to reviews_path
+    else
+      flash[:error] = @review.errors.full_messages
+      render 'new'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :body, :rating)
+  end
+
 end
