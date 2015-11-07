d3c345ce34e44297f60d3c2a8a4a00ddfb8b23fd
Harry Park <imharrypark@gmail.com>
2015-11-06 22:15:24 -0500

    moved user_params to ApplicationController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,4 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
-  end
 end
