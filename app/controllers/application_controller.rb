252f747b354a09091a4b48df6c35f5a7166ba3a4
Harry Park <imharrypark@gmail.com>
2015-11-06 22:30:07 -0500

    create helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
+  helper_method :current_user, :signed_in?
 
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+  def current_user
+    session[:id] ? User.find(session[:id]) : nil
+  end
+
+  def signed_in?
+    current_user != nil
   end
 end
