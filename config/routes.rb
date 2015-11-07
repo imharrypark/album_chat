3d3ed4df655709cc2f42e4fdb0eef66fae56efd3
Harry Park <imharrypark@gmail.com>
2015-11-06 21:37:25 -0500

    create User routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: "reviews#index"
 
+  get '/signin', to: 'users#new'
+
   resources :reviews do
     resources :comments
   end
+
+  resources :users
 end
