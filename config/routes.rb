3bd50d8a4702a0277b91c550eab70abcac749d1a
Harry Park <imharrypark@gmail.com>
2015-11-06 22:16:45 -0500

    create signin route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   root to: "reviews#index"
 
   get '/signup', to: 'users#new'
+  get '/signin', to: 'sessions#new'
 
   resources :reviews do
     resources :comments
   end
 
   resources :users
+
+  resources :sessions, only: [:new, :create, :destroy]
 end
