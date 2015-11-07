bcfc2d1604fefa5b47af5c5e45cc42bbf040ba81
Harry Park <imharrypark@gmail.com>
2015-11-06 22:31:04 -0500

    sign in upon first creating an account.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:id] = @user.id
       redirect_to root_path
     else
       flash[:error] = @user.errors.full_messages
