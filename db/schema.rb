4a79c42af787355fec613f9cca23d539462fdd09
Harry Park <imharrypark@gmail.com>
2015-11-12 18:06:01 -0500

    review table modification

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107022319) do
+ActiveRecord::Schema.define(version: 20151112221854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,11 @@ ActiveRecord::Schema.define(version: 20151107022319) do
     t.text     "body"
     t.float    "rating"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "album_name"
+    t.string   "artist_name"
+    t.string   "album_release_date"
   end
 
   create_table "users", force: :cascade do |t|
